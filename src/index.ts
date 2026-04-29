52c8185416f192bea01cd5e264478ba023b6bcce
Vitor Duarte <vd886032@gmail.com>
2026-04-29 08:51:25 -0300

    aaa

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { cond01 } from "./exercícios/repetição/condicional/q1.js";
+import { cond02 } from "./exercícios/repetição/condicional/q2.js";
+import { cond03 } from "./exercícios/repetição/condicional/q3.js";
+import { cond04 } from "./exercícios/repetição/condicional/q4.js";
+
+
+import { rep01 } from "./exercícios/condicional/q1.js";
+import { rep02 } from "./exercícios/condicional/q2.js";
+import { rep03 } from "./exercícios/condicional/q3.js";
+import { rep04 } from "./exercícios/condicional/q4.js";
+import { rep05 } from "./exercícios/condicional/q5.js";
