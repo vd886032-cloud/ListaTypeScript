bfa9c0a99a3fdb16492b0a8f09aa90dcae02761c
Vitor Duarte <vd886032@gmail.com>
2026-04-29 19:16:59 -0300

    create index.ts

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import {cond01} from './exercícios/condicional/q1.js'
+import {cond02} from './exercícios/condicional/q2.js'
+import {cond03} from './exercícios/condicional/q3.js'
+import {cond04} from './exercícios/condicional/q4.js'
+
+document.getElementById("btnCond01")?.addEventListener("click", cond01)
+document.getElementById("btnCond02")?.addEventListener("click", cond02)
+document.getElementById("btnCond03")?.addEventListener("click", cond03)
+document.getElementById("btnCond04")?.addEventListener("click", cond04)
+
+import {rep01} from './exercícios/repetição/q1.js'
+import {rep02} from './exercícios/repetição/q2.js'
+import {rep03} from './exercícios/repetição/q3.js'
+import {rep04} from './exercícios/repetição/q4.js'
+import {rep05} from './exercícios/repetição/q5.js'
+
+
+document.getElementById("btnRep01")?.addEventListener("click", rep01)
+document.getElementById("btnRep02")?.addEventListener("click", rep02)
+document.getElementById("btnRep03")?.addEventListener("click", rep03)
+document.getElementById("btnRep04")?.addEventListener("click", rep04)
+document.getElementById("btnRep05")?.addEventListener("click", rep05)
+
+import {func01} from './exercícios/funções/q1.js'
+import {func02} from './exercícios/funções/q2.js'
+import {func03} from './exercícios/funções/q3.js'
+import {func04} from './exercícios/funções/q4.js'
+import {func05} from './exercícios/funções/q5.js'
+import {func06} from './exercícios/funções/q6.js'
+import {func07} from './exercícios/funções/q7.js'
+import {func08} from './exercícios/funções/q8.js'
+import {func09} from './exercícios/funções/q9.js'
+import {func10} from './exercícios/funções/q10.js'
+
+document.getElementById("btnFunc01")?.addEventListener("click", func01)
+document.getElementById("btnFunc02")?.addEventListener("click", func02)
+document.getElementById("btnFunc03")?.addEventListener("click", func03)
+document.getElementById("btnFunc04")?.addEventListener("click", func04)
+document.getElementById("btnFunc05")?.addEventListener("click", func05)
+document.getElementById("btnFunc06")?.addEventListener("click", func06)
+document.getElementById("btnFunc07")?.addEventListener("click", func07)
+document.getElementById("btnFunc08")?.addEventListener("click", func08)
+document.getElementById("btnFunc09")?.addEventListener("click", func09)
+document.getElementById("btnFunc10")?.addEventListener("click", func10)
+
+import { arr01 } from './exercícios/Arrays/q1.js'
+import { arr02 } from './exercícios/Arrays/q2.js'
+import { arr03 } from './exercícios/Arrays/q3.js'
+import { arr04 } from './exercícios/Arrays/q4.js'
+import { arr05 } from './exercícios/Arrays/q5.js'
+import { arr06 } from './exercícios/Arrays/q6.js'
+import { arr07 } from './exercícios/Arrays/q7.js'
+import { arr08 } from './exercícios/Arrays/q8.js'
+import { arr09 } from './exercícios/Arrays/q9.js'
+
+
+document.getElementById("btnArr01")?.addEventListener("click", arr01)
+document.getElementById("btnArr02")?.addEventListener("click", arr02)
+document.getElementById("btnArr03")?.addEventListener("click", arr03)
+document.getElementById("btnArr04")?.addEventListener("click", arr04)
+document.getElementById("btnArr05")?.addEventListener("click", arr05)
+document.getElementById("btnArr06")?.addEventListener("click", arr06)
+document.getElementById("btnArr07")?.addEventListener("click", arr07)
+document.getElementById("btnArr08")?.addEventListener("click", arr08)
+document.getElementById("btnArr09")?.addEventListener("click", arr09)
